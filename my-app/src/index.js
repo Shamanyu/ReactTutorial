53d622e3f6b182ea8385537dbde563aa820bc801
shamanyu <shubham.s@greyorange.sg>
2017-11-13 10:11:59 +0530

    Update to add redux, react-redux and redux-devtools and add basic hello world level redux code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -42,4 +42,27 @@ import registerServiceWorker from './registerServiceWorker';
 // ReactDOM.render(<Calculator />, document.getElementById('root'));
 // ReactDOM.render(<WelcomeDialog />, document.getElementById('root'));
 // ReactDOM.render(<PaneApp />, document.getElementById('root'));
-ReactDOM.render(<WelcomeDialogAgain />, document.getElementById('root'));
+// ReactDOM.render(<WelcomeDialogAgain />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+
+function counter(state = 0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
