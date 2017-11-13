f2c0970845d2f6ef261848320d00be63fb7ec453
shamanyu <shubham.s@greyorange.sg>
2017-11-13 12:14:17 +0530

    Add incrementCounter and decrementCounter functions

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -117,6 +117,44 @@ const testRemoveCounter = () => {
   ).toEqual(listAfter);
 };
 
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index]+1,
+    ...list.slice(index+1)
+  ];
+};
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+const decrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index]-1,
+    ...list.slice(index+1)
+  ];
+};
+
+const testDecrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 9, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    decrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
 const store = createStore(counter)
 
 const render = () => {
@@ -143,6 +181,8 @@ render();
 
 testAddCounter();
 testRemoveCounter();
+testIncrementCounter();
+testDecrementCounter();
 console.log('All tests passed');
 
 // store.dispatch({ type: 'INCREMENT' })
