4ada22415bcaf3a19c641c18dbe3744282d2a83e
shamanyu <shubham.s@greyorange.sg>
2017-11-13 10:44:53 +0530

    Comment out self implementation of createStore

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -44,7 +44,7 @@ import registerServiceWorker from './registerServiceWorker';
 // ReactDOM.render(<PaneApp />, document.getElementById('root'));
 // ReactDOM.render(<WelcomeDialogAgain />, document.getElementById('root'));
 
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch(action.type) {
@@ -57,23 +57,23 @@ const counter = (state = 0, action) => {
   }
 }
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-  const getState = () => state;
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter(l => l !== listener);
-    };
-  };
-  dispatch({});
-  return { getState, dispatch, subscribe };
-}
+// const createStore = (reducer) => {
+//   let state;
+//   let listeners = [];
+//   const getState = () => state;
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(listener => listener());
+//   };
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return () => {
+//       listeners = listeners.filter(l => l !== listener);
+//     };
+//   };
+//   dispatch({});
+//   return { getState, dispatch, subscribe };
+// }
 
 const store = createStore(counter)
 
