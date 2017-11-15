0f17b0872a2b0457f0529499bcb122696204e27f
shamanyu <shubham.s@greyorange.sg>
2017-11-15 13:40:58 +0530

    Reorganize code and remove console log statements to log state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -289,11 +289,6 @@ const visibilityFilter = (
 //   };
 // }
 
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-});
-
 const testAddTodo = () => {
   const stateBefore = [];
   const action = {
@@ -359,46 +354,12 @@ testAddTodo();
 testToggleTodo();
 console.log('All tests passed');
 
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
 const store = createStore(todoApp);
 
 console.log('Initial state:');
-console.log(store.getState());
-console.log('----------------');
-
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('----------------');
-
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go shopping'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('----------------');
-
-console.log('Dispatching TOGGLE_TODO.');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 1
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('----------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER.');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED'
-});
-console.log('Current state:');
 console.log(store.getState());
 console.log('----------------');
