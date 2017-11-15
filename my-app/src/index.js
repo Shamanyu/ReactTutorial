54039ed918229adea6d917b1f2b6fc3cdcdfa176
shamanyu <shubham.s@greyorange.sg>
2017-11-15 15:30:24 +0530

    Extract presentational components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -383,26 +383,15 @@ class TodoAppAgain extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-                onClick={() => {
-                  store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id: todo.id
-                  });
-                }}
-                style={{
-                  textDecoration: 
-                    todo.completed ?
-                      'line-through' :
-                      'none'
-                }}
-            >
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          }
+        />
         <p>
           Show:
           {' '}
@@ -431,6 +420,38 @@ class TodoAppAgain extends Component {
     );
   }
 }
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li onClick={onClick}
+      style={{
+        textDecoration: 
+          completed ?
+            'line-through' :
+            'none'
+      }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
 
 const getVisibleTodos = (
   todos,
