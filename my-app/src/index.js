dfc05ceef385818835540160bd02780b2f31f088
shamanyu <shubham.s@greyorange.sg>
2017-11-15 13:35:42 +0530

    Implement combineReducers from scratch

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -274,6 +274,21 @@ const visibilityFilter = (
 //   };
 // }
 
+// const combineReducers = (reducers) => {
+//   return (state = {}, action) => {
+//     return Object.keys(reducers).reduce(
+//       (nextState, key) => {
+//         nextState[key] =reducers[key] (
+//           state[key],
+//           action
+//         );
+//         return nextState;
+//       },
+//       {}
+//     );
+//   };
+// }
+
 const todoApp = combineReducers({
   todos,
   visibilityFilter
