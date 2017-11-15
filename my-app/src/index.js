498653074a2c22151cc24d1012454cf1c14b14a5
shamanyu <shubham.s@greyorange.sg>
2017-11-15 12:19:46 +0530

    Add a reducer to set visibility filters for todoApp

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -248,6 +248,31 @@ const todos = (state = [], action) => {
   }
 };
 
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
+  };
+}
+
 const testAddTodo = () => {
   const stateBefore = [];
   const action = {
@@ -313,10 +338,46 @@ testAddTodo();
 testToggleTodo();
 console.log('All tests passed');
 
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'DECREMENT' })
+const store = createStore(todoApp);
 
-// document.addEventListener('click', () => {
-//   store.dispatch({ type: 'INCREMENT'});
-// });
+console.log('Initial state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 1
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER.');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('----------------');
